9b372f20e2fe16045c1b2fc161869144a07bda08
franky franco <frankyfranco456@gmail.com>
2025-10-15 15:45:12 -0500

    provider de supabase casi completo

--- src/app/core/services/auth/auth.ts
+++ src/app/core/services/auth/auth.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Supabase } from '../../providers/supabase/supabase';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Auth {
-  
+  constructor(private supabasePrv: Supabase) { }
+
+  async register(email: string, password: string) {
+    const { data, error } = await this.supabasePrv.client.auth.signUp({
+      email: email,
+      password: password
+    });
+    if (error) { throw error }
+    return data.user;
+  }
+
+  async login(email: string, password: string) {
+    const {data, error} = await this.supabasePrv.client.auth.signInWithPassword({
+      email : email,
+      password : password
+    });
+    if (error) { throw error }
+    return data.user;
+  }
+
 }
