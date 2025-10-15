1b3537f18dc0c3d781e068b02ed10ffa3c45ddd7
franky franco <frankyfranco456@gmail.com>
2025-10-15 16:13:35 -0500

    correccion menor de problemas y con el provider de autenticacion funcional

--- src/app/core/services/auth/auth.ts
+++ src/app/core/services/auth/auth.ts
@@ -17,12 +17,22 @@ export class Auth {
   }
 
   async login(email: string, password: string) {
-    const {data, error} = await this.supabasePrv.client.auth.signInWithPassword({
-      email : email,
-      password : password
+    const { data, error } = await this.supabasePrv.client.auth.signInWithPassword({
+      email: email,
+      password: password
     });
     if (error) { throw error }
     return data.user;
   }
 
+  async logout() {
+    const { error } = await this.supabasePrv.client.auth.signOut();
+    if (!error) { 
+      return "200";
+    }else{
+      throw error ;
+    }
+  }
+
+  
 }
