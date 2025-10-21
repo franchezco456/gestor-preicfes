bbd752b44e7d64a616402b9202a4c48164cc57ec
soyraf1ta <83525357+Montoya10@users.noreply.github.com>
2025-10-21 10:41:46 -0500

    Revert "Rafa"

--- src/app/shared/shared-module.ts
+++ src/app/shared/shared-module.ts
@@ -1,16 +1,12 @@
-import { input, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CardComponent } from './components/card/card.component';
-import { InputComponent } from './components/input/input.component';
-import { ButtonComponent} from './components/button/button.component';
-import { SelectComponent } from './components/select/select.component';
-import { IonicModule } from '@ionic/angular';
+
+
 
 @NgModule({
-  declarations: [CardComponent, InputComponent, ButtonComponent, SelectComponent],
+  declarations: [],
   imports: [
-    CommonModule, IonicModule
-  ],
-  exports: [CardComponent, InputComponent, ButtonComponent, SelectComponent]
+    CommonModule
+  ]
 })
 export class SharedModule { }
