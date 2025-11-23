3a7030a4a99e105ef00614aca9c18a6f984381e1
JesusRamos01 <jesus.ramosvalois@unicolombo.edu.co>
2025-11-23 16:50:58 -0500

    home nuevo, prueba

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -7,10 +7,15 @@ import { Component, Input, OnInit } from '@angular/core';
   standalone: false,
 })
 export class CardComponent  implements OnInit {
-   @Input () imgSrc : string = '';
-   @Input () title : string = '';
-   @Input () subtitle : string = '';
-   @Input () text : string = '';
+  @Input() imgSrc: string = '';
+  @Input() title: string = '';
+  @Input() subtitle: string = '';
+  @Input() text: string = '';
+  
+  //para las card del home
+  @Input() value: string | number = '';
+  @Input() label: string = '';
+  @Input() variant: 'default' | 'kpi' = 'default';
   constructor() { }
 
   ngOnInit() {}
