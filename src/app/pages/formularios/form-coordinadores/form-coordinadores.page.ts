66bdc4d52efc8cf869ea8a13df293ace2bcaf85c
franky franco <frankyfranco456@gmail.com>
2025-11-23 10:27:38 -0500

    correcccion en obtener las IEs

--- src/app/pages/formularios/form-coordinadores/form-coordinadores.page.ts
+++ src/app/pages/formularios/form-coordinadores/form-coordinadores.page.ts
@@ -72,7 +72,7 @@ export class FormCoordinadoresPage {
   public async getEducationalInstitutions() {
     try {
       await this.loadingSrv.showLoading("Cargando instituciones educativas...");
-      const institutions = await this.querySrv.execute_Function('get_ie_by_cicle', { p_id_cicle: '001' });
+      const institutions = await this.querySrv.execute_Function('get_ie');
       console.log("Instituciones educativas: " + JSON.stringify(institutions));
       this.institutionsOptions = institutions.map((inst: any): SelectOption => ({
         value: inst.id_ie_cicle_out,
